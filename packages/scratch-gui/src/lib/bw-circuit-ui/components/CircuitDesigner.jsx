95543d0bbebc050c503cccafd775b1ccfe4c804f
CrispStrobe <cze+github@mailbox.org>
2026-08-18 07:12:51 +0200

    integrate: packages mirror (max7219 face, SPI fast path, matrix-keypad retarget, rebuilt bundle)

--- packages/scratch-gui/src/lib/bw-circuit-ui/components/CircuitDesigner.jsx
+++ packages/scratch-gui/src/lib/bw-circuit-ui/components/CircuitDesigner.jsx
@@ -1112,7 +1112,6 @@ export function CircuitDesigner({ project, stc, board: externalBoard, debugState
           parts={parts}
           wires={wires}
           theme={theme}
-          lang={lang}
           mode={mode}
           onModeChange={nextMode => {
             setMode(nextMode);
